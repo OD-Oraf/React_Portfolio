e0cfc906304c994bb7fe6592a844e534574fa963
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-02-19 22:38:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+      title: 'OD-Oraf',
+
+      // navbar
+      headerLinks: [
+        {title: 'Home' , path: '/'},
+        {title: 'About' , path: '/about'},
+        {title: 'Contact' , path: '/contact'},
+      ],
+
+      home:{
+        title: 'Be relentless',
+        subTitle: 'Projects that make a difference',
+        text: 'Checkout my projects below'
+      },
+
+      about: {
+        title: 'About me',
+        
+      },
+
+      contact: {
+        title: 'Lets Talk',
+        
+      }
+    }
+  }
+  render(){
+    return (
+
+      <div>
+        Hello from react
+      </div>
+    
+      );
+
+  }
+  
 }
 
 export default App;
