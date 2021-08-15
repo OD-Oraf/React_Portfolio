c98867988b7a08921d2aee76b255dd1d3bf0c000
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2021-08-15 17:25:49 -0400

    remove sticky footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,22 +6,22 @@ import Col from 'react-bootstrap/Col';
 function Footer(){
 
     return(
-        <div className="fixed-bottom">
-            <footer className="mt-5">
-                <Container fluid={true}>
-                    <Row className="border-top justify-content-between p-3">
-                        <Col className= "p-0" md={3} sm= {12}>
-                            OD-Oraf
-                        </Col>
+        
+        <footer className="mt-5">
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
+                    <Col className= "p-0" md={3} sm= {12}>
+                        OD-Oraf
+                    </Col>
 
-                        <Col className="p-0" d-flex justify-content-end md={2}>
-                            Built w/ React-Bootstrap 
-                        </Col>
+                    <Col className="p-0" d-flex justify-content-end md={2}>
+                        Built w/ React-Bootstrap 
+                    </Col>
 
-                    </Row>
-                </Container>
-            </footer>
-        </div>
+                </Row>
+            </Container>
+        </footer>
+        
 
     )
 }
